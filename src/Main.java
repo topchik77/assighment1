8e1eda17008dbd830551fdedd954e1d672fc3f9b
topchik77 <148621778+topchik77@users.noreply.github.com>
2023-12-24 22:48:40 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class MyApplication {
-    public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("C:\\IDEA Folder\\work\\src\\list");
-
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException{
+        File file = new File("C:\\Users\\Adil\\IdeaProjects\\untitled1\\src\\source")
         Scanner sc = new Scanner(file);
         Shape shape = new Shape();
-
-        while (sc.hasNext()) {
+        while (sc.hasNextLine()) {
             double x = sc.nextDouble();
             double y = sc.nextDouble();
-
-            Point point = new Point(x, y);
-            shape.addPoint(point);
+            shape.addPoint(new Point(x,y));
         }
-
         System.out.println(shape.calculatePerimeter());
         System.out.println(shape.getLongest());
+        System.out.println(shape.getAverageSide());
     }
 }
